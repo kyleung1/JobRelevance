9809ad119a65be1acaebad6bceb94d0a6fdb20ac
kyleung1 <92060005+kyleung1@users.noreply.github.com>
2022-12-21 21:20:09 -0800

    evaluate

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -4,8 +4,6 @@ import { useRef, useEffect, useState, RefObject} from 'react';
 import { useRouter } from "next/navigation";
 
 const Search = () => {
-    // const data: RootObject = await getlistings();
-    // console.log(data.page)
     const [search, setSearch] = useState("");
     const [category, setCat] = useState("");
     const [location, setLoc] = useState("");
@@ -30,8 +28,7 @@ const Search = () => {
     }
 
     return (
-        <div>
-            {/* {data.results[0].contents} */}
+        <div className="m-10">
             <form onSubmit={handleSearch} className='flex flex-col'>
                 <label>Choose a job category.</label>
                 <select name="category" className='w-4/12 border-2 border-black rounded-md'
@@ -49,15 +46,15 @@ const Search = () => {
                     <option value="Videography">Videography</option>
                     <option value="Writing%20and%20Editing">Writing and Editing</option>
                 </select>
-                <label>Enter a skill to search for in the chosen job category.</label>
+                <label className="mt-5">Enter a skill to search for in the chosen job category.</label>
                 <input type="text" className="border-2 border-black rounded-md m-0.5 w-4/12" required
                     onChange={(e) => setSearch(e.target.value)}></input>
-                <label><span className="font-semibold text-sky-500">(optional)</span> Enter a location in the format of city, country/state. Example is: 
-                new york, NY.</label>
+                <label className="mt-5"><span className="font-semibold text-sky-500">(optional)</span> Enter a location in the format of city, country/state. 
+                Example is: New York, NY.</label>
                 <input type="text" className="border-2 border-black rounded-md m-0.5 w-4/12"
                     onChange={(e) => setLoc(e.target.value)}></input>
                 <button className='p-3 w-2/12 text-white border-2 rounded-md bg-sky-500 
-                    hover:bg-sky-700 hover:cursor-pointer'>Evaluate!</button>
+                   mt-5 hover:bg-sky-700 hover:cursor-pointer'>Evaluate!</button>
             </form>
             
         </div>
@@ -66,20 +63,4 @@ const Search = () => {
 
 export default Search
 
-// https://www.themuse.com/api/public/jobs?category=Software%20Engineering&location=Seattle%2C%20WA&page=3&api_key=3c366481db39baeef30e68f8883148d9cb4cf1a2846416fc53186df135f3636a
 // e => to get type
-// 3815
-
-// async function getlistings() {
-//     try{
-//         const res1 = await fetch(API + '?category=Software%20Engineering&location=Seattle%2C%20WA&page=3&api_key=' +
-//         process.env.THE_MUSE, {cache: "no-store"});
-//         if (!res1.ok) {
-//             throw new Error("Failed to fetch data")
-//         }
-//         const jobs: RootObject = await res1.json();
-//         return jobs;
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
--- app/search/[evalTerms]/page.tsx
+++ app/search/[evalTerms]/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RootObject } from '../../../types';
+import { Result, RootObject } from '../../../types';
 
 type PageProps = {
   params: {
@@ -9,8 +9,8 @@ type PageProps = {
 
 //https://www.themuse.com/developers/api/v2
 const API = "https://www.themuse.com/api/public/jobs";
-// let full_API = "";
-// full_API = "";
+
+let searchG = "";
 
 async function getlistings(evalTerms: string) {
     try{
@@ -18,29 +18,59 @@ async function getlistings(evalTerms: string) {
         const search = evalTermsSplit[0];
         const category = evalTermsSplit[1];
         let location = "";
-        if (evalTermsSplit.length > 3) {
+        if (evalTermsSplit.length === 3) {
           location = evalTermsSplit[2];
         }
+        searchG = search;
         const APILOC = API + "?category=" + category + "&location=" + location + "&page=1&api_key=" + process.env.THE_MUSE;
         const APINOLOC = API + "?category=" + category + "&page=1&api_key=" + process.env.THE_MUSE;
-        const res1 = await fetch(API + '?category=Software%20Engineering&location=Seattle%2C%20WA&page=3&api_key=' +
-        process.env.THE_MUSE, {cache: "no-store"});
-        if (!res1.ok) {
-            throw new Error("Failed to fetch data")
+        if (evalTermsSplit.length === 3) {
+          const res1 = await fetch(APILOC, {cache: "no-store"});
+          const jobs: RootObject = await res1.json();
+          return jobs;
+        } else {
+          const res1 = await fetch(APINOLOC, {cache: "no-store"});
+          const jobs: RootObject = await res1.json();
+          return jobs;
         }
-        const jobs: RootObject = await res1.json();
-        return jobs;
     } catch (err) {
         console.log(err)
     }
 }
 
+function evaluateSkill(listings: RootObject) {
+  const search = searchG;
+  let re: RegExp = new RegExp(search, "gi");
+  let total = 0;
+  for (let i = 0; i < listings.results.length; i++) {
+    let content = listings.results[i].contents;
+    let matches: RegExpMatchArray | null = content.match(re);
+    let count: number = matches ? matches.length : 0;
+    total += count;
+  }
+  return total;
+}
+
 const results = async ({ params: {evalTerms}}: PageProps) => {
-  // const evalResults = await getlistings(evalTerms);
+  const listings = await getlistings(evalTerms);
+  let evalResults;
+  if (listings?.timed_out !== undefined) {
+    evalResults = evaluateSkill(listings);
+  }
+  const evalTermsSplit = evalTerms.split("-");
+  let category = evalTermsSplit[1];
+  let location = "a general search";
+  if (evalTermsSplit.length === 3) {
+    location = evalTermsSplit[2];
+  }
   return (
-    <div>
+    <div className="m-10">
       <h1>Results</h1>
-      {evalTerms}
+      <p>
+        The term <span className="text-orange-500">{searchG}</span> appeared <span className="text-orange-500">
+          {evalResults}</span> times while searching through <span className="text-orange-500">{category}</span> job 
+          listings in <span className="text-orange-500">{location}</span>.
+      </p>
     </div>
   )
 }
